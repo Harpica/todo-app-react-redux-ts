7394d1d770b02c6282830e9b59fd93d1eec2cf53
Harpica <harpy.ka@gmail.com>
2023-02-11 00:33:45 +0300

    feat: add isLoading state

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
@@ -7,16 +7,18 @@ import { AppThunkDispatch, getTodosAsync, TodoState } from "../redux/todoSlice";
 import TodoItem from "./TodoItem";
 
 const TodoList = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const dispatch = useDispatch<AppThunkDispatch>();
   const todos: Array<TodoState> = useSelector((state: RootState) => state.todos);
 
   useEffect(() => {
-    dispatch(getTodosAsync());
+    setIsLoading(true);
+    dispatch(getTodosAsync()).finally(()=> setIsLoading(false));
   }, [dispatch])
 
   return (
     <ul className="list-group">
-      {todos.map((todo, i) => (
+      {!isLoading && todos.map((todo, i) => (
         <TodoItem
           key={i}
           id={todo.id}
